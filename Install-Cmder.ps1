2a378da4d0955e9c083fc447c8122d4183c19c1d
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-06-05 11:08:21 +0100

    Initial Commit.  Script just contains comments of steps to write.`nCmder.xml is the exported Scheduled Task.`nConEmu.xml is a backup of the settings file.

---
+++ Install-Cmder.ps1
@@ -0,0 +1,11 @@
+# Identify latest release from: https://github.com/cmderdev/cmder/releases/latest
+
+# Download cmder.zip
+
+# Extract cmder.zip to C:\Program Files\Cmder\
+
+# Copy ConEmu.xml to C:\Program Files\cmder\vendor\conemu-maximus5\ConEmu.xml
+
+# Import Scheduled Task (to start Cmder) using Cmder.xml
+## This involves changing some details in Cmder.xml such as LogonTrigger\UserId
+## Might also involve changing Author, some testing is required.
