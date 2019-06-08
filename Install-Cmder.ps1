98cb9d72246bfb0d826399db5e9660a00b88d524
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-06-08 12:58:24 +0100

    Fixed broken creation of scheduled task

--- Install-Cmder.ps1
+++ Install-Cmder.ps1
@@ -12,6 +12,11 @@ Copy-Item -Path .\ConEmu.xml -Destination "C:\Program Files\cmder\vendor\conemu-
 ## This involves changing some details in Cmder.xml such as LogonTrigger\UserId
 ## Might also involve changing Author, some testing is required.
 #$SID = (New-Object System.Security.Principal.NTAccount($ENV:USERDOMAIN,$ENV:USERNAME)).Translate([System.Security.Principal.SecurityIdentifier]).Value
-$trigger = New-ScheduledTaskTrigger -AtLogOn -User "$ENV:USERDOMAIN\$ENV:USERNAME"
-Register-ScheduledTask -TaskName Cmder -Xml (Get-Content .\Cmder.xml | Out-String) -User "$ENV:USERDNSDOMAIN\$ENV:USERNAME" -Trigger $trigger -Force
+
+$taskAction = New-ScheduledTaskAction -Execute "C:\Program Files\cmder\Cmder.exe"
+$taskTrigger = New-ScheduledTaskTrigger -AtLogon -User "$ENV:USERDOMAIN\$ENV:USERNAME"
+$taskPrincipal = New-ScheduledTaskPrincipal "Contoso\Administrator"
+$taskSettings = New-ScheduledTaskSettingsSet -DisallowHardTerminate -AllowStartIfOnBatteries -MultipleInstances IgnoreNew -RestartCount 0 -DontStopIfGoingOnBatteries
+$schTask = New-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Principal $taskPrincipal -Settings $taskSettings
+Register-ScheduledTask -TaskName Cmder -InputObject $schTask -User "$ENV:USERDOMAIN\$ENV:USERNAME"
 Start-ScheduledTask -TaskName Cmder
